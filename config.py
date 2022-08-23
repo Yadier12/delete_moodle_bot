4220737811763e353cc9593e6e12f7f93e3ffe33
Yadier12 <110910998+Yadier12@users.noreply.github.com>
2022-08-23 10:44:10 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-api_id = 
-api_hash = 'API HASH AQUI'
-bot_token = 'BOT TOKEN AQUI'
+api_id = 17345593
+api_hash = 'AQUI80ae053901c049b519ea5bf0f50e1675'
+bot_token = '5648943656:AAEzhhyqo72-b82hZK7twS0q3Z7mTqezhaU'
